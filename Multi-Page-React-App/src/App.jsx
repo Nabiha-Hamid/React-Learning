1e2812387b89b7b74bb61b3ba6dbcffd0aceee8f
Nabiha <nabiha.hamid@devsinc.com>
2025-05-20 21:48:28 +0500

    Multi-Page-App

---
+++ Multi-Page-React-App/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
+
+
+
+function App() {
+    const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <><Navbar /><Home /></>
+    },
+    {
+      path: "/login",
+      element: <><Navbar /><Login /></>
+    },
+    
+  ])
+  return (
+    <>
+    <div>hee heheheehhe </div>
+  
+     <RouterProvider router = {router}/>
+    </>
+  )
+}
+
+export default App
